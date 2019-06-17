705a3d713e92b2635458df62c5fe257c798d6621
dangkhoagms <dongdangkhoa97@gmail.com>
2019-06-17 09:41:43 +0700

    Finished Course

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -1,25 +1,44 @@
 <?php
+
 namespace Dino\play;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-require __DIR__.'/../vendor/autoload.php';
-$container = new ContainerBuilder();
-$container->setParameter('root_dir',__DIR__);
+require __DIR__ . '/../vendor/autoload.php';
+
 
 $start = microtime(true);
 
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
-$loader->load('services.yml');
+$cacheContainer = __DIR__ . '/cached_container.php';
+if (!file_exists($cacheContainer)) {
+    $container = new ContainerBuilder();
+    $container->setParameter('root_dir', __DIR__);
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+    $loader->load('services.yml');
 
-$container->compile();
+
+    $container->compile();
+
+
+    $dumper = new PhpDumper($container);
+    file_put_contents(__DIR__ . '/cached_container.php', $dumper->dump());
+
+}
+
+require $cacheContainer;
+$container = new \ProjectServiceContainer();
+
 runApp($container);
 
-$elapsed = round((microtime(true) - $start) * 1000);
-$container->get('logger')->debug('Elapsed Time: '.$elapsed.'ms');
 
-function runApp(ContainerBuilder $builder){
+$elapsed = round((microtime(true) - $start) * 1000);
+$container->get('logger')->debug('Elapsed Time: ' . $elapsed . 'ms');
+
+function runApp(ContainerInterface $builder)
+{
     $builder->get('logger')->info('AAAA');
 }
